049e3eddf6b47db96bfa582fc3579ddf54ed1f21
Qirashi <90517227+qirashi@users.noreply.github.com>
2026-02-18 19:55:30 +0400

    dvpl_go 1.4.1
    - Обновлён go до 1.26.0
    - Небольшие правки в коде.
    - Отказ от "bytes"

--- _build_.bat
+++ _build_.bat
@@ -6,6 +6,9 @@
 echo off
 chcp 65001
 
+echo Модернизация кода...
+go fix ./...
+
 echo Начинаю сборку.
 set CGO_ENABLED=1
 go build -o ./out/dvpl.exe -buildvcs=false -ldflags="-s -w -buildid=" -trimpath -buildmode=exe -tags=release -asmflags="-trimpath" -mod=readonly dvpl_go.go
@@ -14,7 +17,6 @@ if %ERRORLEVEL% neq 0 (
     exit /b %ERRORLEVEL%
 )
 echo Сборка выполнена успешно.
-
 
 where ResourceHacker >nul 2>nul
 if %errorlevel% == 0 (
